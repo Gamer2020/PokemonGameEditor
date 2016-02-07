1277615006e3e4b92697f965716d8894f5dc8c2e
Gamer2020 <lostatlife2020@hotmail.com>
2016-02-07 02:28:27 -0500

    Pokemon Adder: Repoints Dex flags
    It repoints the dex flags. I wanted to sync my changes so I don't lose
    them.

--- GBAPokemonGameEditor/PokemonAdder.Designer.vb
+++ GBAPokemonGameEditor/PokemonAdder.Designer.vb
@@ -25,11 +25,21 @@ Partial Class PokemonAdder
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(PokemonAdder))
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
         Me.TabControl1.SuspendLayout()
         Me.TabPage1.SuspendLayout()
         Me.SuspendLayout()
@@ -41,11 +51,21 @@ Partial Class PokemonAdder
         Me.TabControl1.Location = New System.Drawing.Point(12, 21)
         Me.TabControl1.Name = "TabControl1"
         Me.TabControl1.SelectedIndex = 0
-        Me.TabControl1.Size = New System.Drawing.Size(302, 227)
+        Me.TabControl1.Size = New System.Drawing.Size(302, 381)
         Me.TabControl1.TabIndex = 0
         '
         'TabPage1
         '
+        Me.TabPage1.Controls.Add(Me.CheckBox2)
+        Me.TabPage1.Controls.Add(Me.Label7)
+        Me.TabPage1.Controls.Add(Me.Label6)
+        Me.TabPage1.Controls.Add(Me.TextBox3)
+        Me.TabPage1.Controls.Add(Me.Label5)
+        Me.TabPage1.Controls.Add(Me.TextBox2)
+        Me.TabPage1.Controls.Add(Me.TextBox1)
+        Me.TabPage1.Controls.Add(Me.Label4)
+        Me.TabPage1.Controls.Add(Me.Label3)
+        Me.TabPage1.Controls.Add(Me.Label2)
         Me.TabPage1.Controls.Add(Me.Label1)
         Me.TabPage1.Controls.Add(Me.Button1)
         Me.TabPage1.Controls.Add(Me.LinkLabel1)
@@ -53,52 +73,10 @@ Partial Class PokemonAdder
         Me.TabPage1.Location = New System.Drawing.Point(4, 22)
         Me.TabPage1.Name = "TabPage1"
         Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(294, 201)
+        Me.TabPage1.Size = New System.Drawing.Size(294, 355)
         Me.TabPage1.TabIndex = 0
         Me.TabPage1.Text = "Fire Red"
         Me.TabPage1.UseVisualStyleBackColor = True
-        '
-        'TabPage2
-        '
-        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(235, 123)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "Emerald"
-        Me.TabPage2.UseVisualStyleBackColor = True
-        '
-        'CheckBox1
-        '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Checked = True
-        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
-        Me.CheckBox1.Location = New System.Drawing.Point(14, 41)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(185, 17)
-        Me.CheckBox1.TabIndex = 0
-        Me.CheckBox1.Text = "Install JPAN's  " & Global.Microsoft.VisualBasic.ChrW(9) & "save block recycle"
-        Me.CheckBox1.UseVisualStyleBackColor = True
-        '
-        'LinkLabel1
-        '
-        Me.LinkLabel1.Location = New System.Drawing.Point(11, 118)
-        Me.LinkLabel1.Name = "LinkLabel1"
-        Me.LinkLabel1.Size = New System.Drawing.Size(265, 69)
-        Me.LinkLabel1.TabIndex = 1
-        Me.LinkLabel1.TabStop = True
-        Me.LinkLabel1.Text = "To remain consistent with what other people may have already done this tool uses " &
-    "the research from the folowing thread: http://www.pokecommunity.com/showthread.p" &
-    "hp?t=318569"
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(201, 81)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 2
-        Me.Button1.Text = "Add"
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'Label1
         '
@@ -109,11 +87,141 @@ Partial Class PokemonAdder
         Me.Label1.TabIndex = 3
         Me.Label1.Text = "Number of Pokemon in ROM:"
         '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(202, 248)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Add"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'LinkLabel1
+        '
+        Me.LinkLabel1.Location = New System.Drawing.Point(12, 274)
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.Size = New System.Drawing.Size(265, 69)
+        Me.LinkLabel1.TabIndex = 1
+        Me.LinkLabel1.TabStop = True
+        Me.LinkLabel1.Text = "To remain consistent with what other people may have already done this tool uses " &
+    "the research from the folowing thread: http://www.pokecommunity.com/showthread.p" &
+    "hp?t=318569"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(14, 140)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(185, 17)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Install JPAN's  " & Global.Microsoft.VisualBasic.ChrW(9) & "save block recycle"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(294, 201)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Emerald"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(11, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(154, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Number of Dex entries in ROM:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(11, 84)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(148, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Number of Dex entries to add:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(11, 40)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(140, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Number of Pokemon to add:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(167, 37)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox1.TabIndex = 7
+        Me.TextBox1.Text = "100"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(167, 81)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox2.TabIndex = 8
+        Me.TextBox2.Text = "100"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(11, 166)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(135, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Base offset to use for flags:"
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(167, 163)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(67, 20)
+        Me.TextBox3.TabIndex = 10
+        Me.TextBox3.Text = "0203C0C8"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(11, 192)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(103, 13)
+        Me.Label6.TabIndex = 11
+        Me.Label6.Text = "Seen flags location: "
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(11, 217)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(109, 13)
+        Me.Label7.TabIndex = 12
+        Me.Label7.Text = "Caught flags location:"
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Location = New System.Drawing.Point(14, 109)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(100, 17)
+        Me.CheckBox2.TabIndex = 13
+        Me.CheckBox2.Text = "Delete Old data"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
         'PokemonAdder
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(326, 251)
+        Me.ClientSize = New System.Drawing.Size(323, 414)
         Me.Controls.Add(Me.TabControl1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
@@ -135,4 +243,14 @@ Partial Class PokemonAdder
     Friend WithEvents LinkLabel1 As LinkLabel
     Friend WithEvents Button1 As Button
     Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents CheckBox2 As CheckBox
 End Class
--- GBAPokemonGameEditor/PokemonAdder.vb
+++ GBAPokemonGameEditor/PokemonAdder.vb
@@ -1,4 +1,5 @@
 ﻿Imports System.IO
+Imports VB = Microsoft.VisualBasic
 
 Public Class PokemonAdder
     Private Sub PokemonAdder_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -19,7 +20,11 @@ Public Class PokemonAdder
             TabControl1.SelectedIndex = 0
 
             Label1.Text = "Number of Pokemon in ROM: " & (GetString(GetINIFileLocation(), header, "NumberOfPokemon", "")) - 1
+            Label2.Text = "Number of Dex entries in ROM: " & (GetString(GetINIFileLocation(), header, "NumberOfDexEntries", "")) - 1
 
+
+            Label6.Text = "Seen flags location: " & Hex(CInt("&H" & TextBox3.Text))
+            Label7.Text = "Caught flags location: " & Hex((("&H" & TextBox3.Text) + Math.Ceiling((((GetString(GetINIFileLocation(), header, "NumberOfDexEntries", "")) - 1) + TextBox2.Text) / 8)))
 
         Else
 
@@ -72,12 +77,62 @@ Public Class PokemonAdder
 
         End If
 
+        'This makes the Pokedex work with the save hack.
+
+        If CheckBox1.Checked Then
+
+            'Disable LR help menu
+            WriteHEX(LoadedROM, &H13B8C2, "1DE0")
+
+            'Makes game be able to read the new location of the SEEN flags.
+
+            WriteHEX(LoadedROM, &H104B10, ReverseHEX(VB.Right("00000000" & Hex(CInt("&H" & TextBox3.Text)), 8)))
+            WriteHEX(LoadedROM, &H104B00, "0000")
+            WriteHEX(LoadedROM, &H104B02, "0000")
+
+            'Makes game be able to read the new location of the CAUGHT flags.
+
+            WriteHEX(LoadedROM, &H104B5C, ReverseHEX(VB.Right("00000000" & Hex((("&H" & TextBox3.Text) + Math.Ceiling((((GetString(GetINIFileLocation(), header, "NumberOfDexEntries", "")) - 1) + TextBox2.Text) / 8))), 8)))
+            WriteHEX(LoadedROM, &H104B16, "0000")
+            WriteHEX(LoadedROM, &H104B18, "0000")
+            WriteHEX(LoadedROM, &H104B1A, "0000")
+            WriteHEX(LoadedROM, &H104B26, "16E0")
+
+            'This makes both of the above bypass redundant SEEN flags.??
+            WriteHEX(LoadedROM, &H104B34, "0FE0")
+
+            'This makes the game write to the New SEEN flags.
+            WriteHEX(LoadedROM, &H104B94, ReverseHEX(VB.Right("00000000" & Hex(CInt("&H" & TextBox3.Text)), 8)))
+            WriteHEX(LoadedROM, &H104B6A, "011C")
+            WriteHEX(LoadedROM, &H104B6C, "0000")
+            WriteHEX(LoadedROM, &H104B78, "1AE0")
+
+            'This makes the game write to the New CAUGHT flags.
+            WriteHEX(LoadedROM, &H104BB8, ReverseHEX(VB.Right("00000000" & Hex((("&H" & TextBox3.Text) + Math.Ceiling((((GetString(GetINIFileLocation(), header, "NumberOfDexEntries", "")) - 1) + TextBox2.Text) / 8))), 8)))
+            WriteHEX(LoadedROM, &H104BA2, "011C")
+            WriteHEX(LoadedROM, &H104BA4, "0000")
+
+            'Makes the game clear the flags properly when the player selects New Game at the main menu.
+            WriteHEX(LoadedROM, &H549D0, ReverseHEX(VB.Right("00000000" & Hex(CInt("&H" & TextBox3.Text)), 8)))
+            WriteHEX(LoadedROM, &H549B0, "201C0000")
+            WriteHEX(LoadedROM, &H549B6, Hex(Math.Ceiling((((GetString(GetINIFileLocation(), header, "NumberOfDexEntries", "")) - 1) + TextBox2.Text) / 8)))
+            WriteHEX(LoadedROM, &H549B7, "22")
+            WriteHEX(LoadedROM, &H549BC, "201C")
+            WriteHEX(LoadedROM, &H549BE, Hex(Math.Ceiling((((GetString(GetINIFileLocation(), header, "NumberOfDexEntries", "")) - 1) + TextBox2.Text) / 8)))
+            WriteHEX(LoadedROM, &H549BF, "30")
+            WriteHEX(LoadedROM, &H549C2, Hex(Math.Ceiling((((GetString(GetINIFileLocation(), header, "NumberOfDexEntries", "")) - 1) + TextBox2.Text) / 8)))
+            WriteHEX(LoadedROM, &H549C3, "22")
+
+
+        End If
+
 
         Cursor = Cursors.WaitCursor
 
         Cursor = Cursors.Arrow
 
         Label1.Text = "Number of Pokemon in ROM: " & (GetString(GetINIFileLocation(), header, "NumberOfPokemon", "")) - 1
+        Label2.Text = "Number of Dex entries in ROM: " & (GetString(GetINIFileLocation(), header, "NumberOfDexEntries", "")) - 1
 
         MsgBox("Pokemon added successfully!")
 
@@ -85,5 +140,153 @@ Public Class PokemonAdder
 
     Private Sub LinkLabel1_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
         Process.Start("http://www.pokecommunity.com/showthread.php?t=318569")
+    End Sub
+
+    Private Sub TextBox3_KeyPress(sender As Object, e As KeyPressEventArgs) Handles TextBox3.KeyPress
+
+        If e.KeyChar = "0" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "1" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "2" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "3" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "4" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "5" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "6" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "6" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "7" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "8" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "9" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "A" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "B" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "C" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "D" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "E" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "F" Then
+            Exit Sub
+        End If
+
+        If e.KeyChar = "a" Then
+            e.KeyChar = "A"
+            Exit Sub
+        End If
+
+        If e.KeyChar = "b" Then
+            e.KeyChar = "B"
+            Exit Sub
+        End If
+
+        If e.KeyChar = "c" Then
+            e.KeyChar = "C"
+            Exit Sub
+        End If
+
+        If e.KeyChar = "d" Then
+            e.KeyChar = "D"
+            Exit Sub
+        End If
+
+        If e.KeyChar = "e" Then
+            e.KeyChar = "E"
+            Exit Sub
+        End If
+
+        If e.KeyChar = "f" Then
+            e.KeyChar = "F"
+            Exit Sub
+        End If
+
+        If e.KeyChar = Convert.ToChar(Keys.Back) Then
+            Exit Sub
+        End If
+
+        e.KeyChar = ""
+
+    End Sub
+
+    Private Sub TextBox3_TextChanged(sender As Object, e As EventArgs) Handles TextBox3.TextChanged
+        If TextBox3.Text = "" Then
+            TextBox3.Text = 0
+        End If
+
+        Label6.Text = "Seen flags location: " & Hex(CInt("&H" & TextBox3.Text))
+        Label7.Text = "Caught flags location: " & Hex((("&H" & TextBox3.Text) + Math.Ceiling((((GetString(GetINIFileLocation(), header, "NumberOfDexEntries", "")) - 1) + TextBox2.Text) / 8)))
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+
+        If TextBox2.Text = "" Then
+            TextBox2.Text = 0
+        End If
+
+        If TextBox3.Text <> "" Then
+
+            Label6.Text = "Seen flags location: " & Hex(CInt("&H" & TextBox3.Text))
+            Label7.Text = "Caught flags location: " & Hex((("&H" & TextBox3.Text) + Math.Ceiling((((GetString(GetINIFileLocation(), header, "NumberOfDexEntries", "")) - 1) + TextBox2.Text) / 8)))
+
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+        If CheckBox1.Checked = True Then
+            TextBox3.Enabled = True
+        End If
+
+        If CheckBox1.Checked = False Then
+            TextBox3.Enabled = False
+        End If
+
     End Sub
 End Class
